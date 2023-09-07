4c3473703606fb571b4378993ab5aae3f25fb99a
Marcin Jasiukowicz <hi@y4s.io>
2023-09-07 21:35:30 +0200

    add support for 3D .terrain tiles

--- scraper.py
+++ scraper.py
@@ -8,16 +8,23 @@ from random import random
 
 from utils import tile2latlon, latlon2tile
 
-BASE_WAIT = 0.5
+headers = {
+        "Authorization": ""
+#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MWU2OTlhYi1hZWI1LTQwYjAtYjQzZi1mN2NhZTc3YWRjOGMiLCJpZCI6MjU5LCJhc3NldHMiOnsiMSI6eyJ0eXBlIjoiVEVSUkFJTiIsImV4dGVuc2lvbnMiOlt0cnVlLHRydWUsdHJ1ZV0sInB1bGxBcGFydFRlcnJhaW4iOmZhbHNlfX0sInNyYyI6Ijc4NmQwNDM5LTdkYmMtNDNlZS1iOWZjLThmYzljZTA3M2EyZiIsImlhdCI6MTY5NDA0NDg2NiwiZXhwIjoxNjk0MDQ4NDY2fQ.QSwXx0xrG7XL1S_suT93vwTwioZO5dF1Ja8nIyCznbU"
+}
+
+
+BASE_WAIT = 0.01
 
 def parse_args():
     parser = argparse.ArgumentParser(description='scrape tiles from a tiled map service')
     parser.add_argument('--poly', required=True, type=str, help='path to geojson containnig bounding polygon(s) to scrape within in lat lon')
     parser.add_argument('--zoom', required=True, type=int, help='zoom level to scrape at')
     parser.add_argument('--url', required=True, type=str, help='Map service url in http://...{z}/{x}/{y}... format')
+    parser.add_argument('--ext', required=True, type=str, help='Extension for the downloaded file')
     parser.add_argument('--out-dir', required=True, type=str, help='Folder to output to')
-    parser.add_argument('--max-connections', required=False, type=int, default=8, help='Max concurrent connections')
-    parser.add_argument('--retries', required=False, type=int, default=10, help='Retries per tile')
+    parser.add_argument('--max-connections', required=False, type=int, default=20, help='Max concurrent connections')
+    parser.add_argument('--retries', required=False, type=int, default=1, help='Retries per tile')
     opts = parser.parse_args()
 
     with open(opts.poly, 'r') as geojf:
@@ -46,15 +53,15 @@ def tile_idxs_in_poly(poly : shapely.geometry.Polygon, zoom : int):
 async def fetch_and_save(session : aiohttp.ClientSession, url : str, retries : int, filepath : str, **kwargs):
     wait_for = BASE_WAIT
     for retry in range(retries):
-        response = await session.get(url, params=kwargs)
+        response = await session.get(url, **kwargs)
         try:
             response.raise_for_status()
             img = await response.read()
             with open(filepath, 'wb') as fp:
                 fp.write(img)
             return True
-        except aiohttp.client_exceptions.ClientResponseError:
-            print('err')
+        except aiohttp.client_exceptions.ClientResponseError as e:
+            print(e)
             await asyncio.sleep(wait_for)
             wait_for = wait_for * (1.0 * random() + 1.0)
     return False
@@ -63,10 +70,28 @@ async def main():
     failed_urls = []
 
     opts = parse_args()
+
+    if not os.path.exists(opts.out_dir):
+        os.makedirs(opts.out_dir)
+
+    if opts.ext=="terrain":
+        ## Download manifest, get all we need, and save it to file while changing absolute online URL to relative local one
+        manifest_path = os.path.join(opts.out_dir, "layer.json")
+        async with aiohttp.ClientSession() as session:
+            await fetch_and_save(session, opts.url, opts.retries, manifest_path)
+        with open(manifest_path) as layer:
+            manifest = json.load(layer)
+            url = manifest["tiles"][0]
+            opts.url = url
+            offsets = manifest["available"]
+            local_url = f"/{opts.out_dir}/{'/'.join(url.split('/')[-3:]).split('?')[0]}"
+            print(f"Local tiles path: {local_url}")
+            manifest["tiles"][0] = local_url
+        
+        with open(manifest_path, "w") as outfile:
+            json.dump(manifest, outfile)
     
     os.makedirs(opts.out_dir, exist_ok=True)
-
-    semaphore = asyncio.Semaphore(opts.max_connections)
 
     for feat in opts.poly['features']:
         poly = shapely.geometry.shape(feat['geometry'])
@@ -74,15 +99,32 @@ async def main():
         async with aiohttp.ClientSession() as session:
             tasks = []
             urls = []
-            for x, y in tile_idxs_in_poly(poly, opts.zoom):
+            z = opts.zoom
+            if opts.ext == "terrain":
+                z = opts.zoom+1
+            for x, y in tile_idxs_in_poly(poly, z):
+                if opts.ext == "terrain":
+                    offsets_z=offsets[opts.zoom][0]
+                    # print(f"found offsets for layer {opts.zoom}: x:{startX} y:{startY}")
+                    # print(f"found max values for layer {opts.zoom}: x:{endX} y:{endY}")
+                    x = x + offsets_z['startX']
+                    y = y + offsets_z['startY']
+                    if(y > offsets_z['endY']): continue
+                    if(x > offsets_z['endX']): continue
+                    #y = (2 ** opts.zoom) - y - 1 + offset
                 url = opts.url.format(z=opts.zoom, x=x, y=y)
-                with (await semaphore):
-                    filepath = os.path.join(opts.out_dir, '{}_{}_{}.png'.format(opts.zoom, x, y))
-                    if os.path.isfile(filepath):
-                        continue
-                    ret = fetch_and_save(session, url, opts.retries, filepath)
-                    urls.append(url)
-                    tasks.append(asyncio.ensure_future(ret))
+                print("Downloading %s" % url)
+                #async with asyncio.Semaphore(opts.max_connections):
+                dirpath = os.path.join(opts.out_dir, '{}/{}/'.format(opts.zoom, x))
+                filepath = os.path.join(opts.out_dir, '{}/{}/{}.{}'.format(opts.zoom, x, y, opts.ext))
+                if not os.path.exists(dirpath):
+                    os.makedirs(dirpath)
+                if os.path.isfile(filepath):
+                    continue
+                ret = fetch_and_save(session, url, opts.retries, filepath, headers=headers)
+                urls.append(url)
+                tasks.append(asyncio.ensure_future(ret))
+                
             
             res : list = await asyncio.gather(*tasks)
             n_failed = res.count(False)
